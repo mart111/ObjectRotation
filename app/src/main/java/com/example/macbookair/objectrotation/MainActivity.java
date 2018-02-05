4ed3717ea676f1235780a82a217aa613436b1133
Martin <mart34111@gmail.com>
2018-02-05 21:09:07 +0400

    final commit

--- app/src/main/java/com/example/macbookair/objectrotation/MainActivity.java
+++ app/src/main/java/com/example/macbookair/objectrotation/MainActivity.java
@@ -1,24 +1,22 @@
 package com.example.macbookair.objectrotation;
 
-import android.graphics.Canvas;
-import android.graphics.Color;
-import android.graphics.Matrix;
-import android.graphics.Paint;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.view.GestureDetectorCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.view.GestureDetector;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.RelativeLayout;
-import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity implements View.OnTouchListener, View.OnClickListener {
+public class MainActivity extends AppCompatActivity implements GestureDetector.OnGestureListener, View.OnClickListener {
 
     private View rectangle;
     private RelativeLayout mainLayout;
-    private float newX, newY;
-    private TextView txt;
+    private float x1, x2;
     private Button btnReset;
+    private GestureDetectorCompat gestureDetectorCompat;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,37 +24,71 @@ public class MainActivity extends AppCompatActivity implements View.OnTouchListe
         setContentView(R.layout.activity_main);
 
         rectangle = findViewById(R.id.rectangle);
-        txt = findViewById(R.id.txt);
         btnReset = findViewById(R.id.resetBtn);
         mainLayout = findViewById(R.id.mainLayout);
 
-        mainLayout.setOnTouchListener(this);
+        gestureDetectorCompat = new GestureDetectorCompat(this, this);
         btnReset.setOnClickListener(this);
+    }
 
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                x1 = event.getX();
+                break;
+            case MotionEvent.ACTION_UP:
+                x2 = event.getX();
+                float delta = x2 - x1;
+
+                if (Math.abs(delta) > 0) {
+                    // Left to Right swipe action
+                    if (x2 > x1) {
+                        rectangle.setRotationY(delta);
+                    }
+
+                    // Right to left swipe action
+                    else {
+                        rectangle.setRotationY(Math.abs(x1 - x2));
+                    }
+                }
+        }
+        return super.onTouchEvent(event);
+    }
+
+    @Override
+    public boolean onDown(MotionEvent e) {
+        return false;
+    }
+
+    @Override
+    public void onShowPress(MotionEvent e) {
 
     }
 
     @Override
-    public boolean onTouch(View v, MotionEvent event) {
-
-        newX = event.getX() - v.getX();
-        newY = event.getY() - v.getY();
-
-        rectangle.setRotationX(newX);
-        rectangle.setRotationY(newY);
-
-
-        txt.setText("X= " + newX + "\n" + "Y= " + newY + "\n" + "Angle= " + rectangle.getRotation());
-
-
-        return true;
+    public boolean onSingleTapUp(MotionEvent e) {
+        return false;
     }
 
+    @Override
+    public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX, float distanceY) {
+
+        return false;
+    }
+
+    @Override
+    public void onLongPress(MotionEvent e) {
+
+    }
+
+    @Override
+    public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
+        return false;
+    }
 
     @Override
     public void onClick(View v) {
-        rectangle.setRotationX(0);
         rectangle.setRotationY(0);
-        txt.setText("");
     }
 }
